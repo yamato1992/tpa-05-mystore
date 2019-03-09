aedde121d1ed8c01ba2289e124ef4d542ab9ae3e
yamato mitsui <yamato.mitsui.orleans@gmail.com>
2019-03-09 12:09:05 +0900

    modify db schema

--- server/db/items.sql
+++ server/db/items.sql
@@ -1,11 +1,11 @@
 USE `my_store`;
 
-INSERT INTO `items` (`name`, `price`) VALUES ('Item 1', 100);
-INSERT INTO `items` (`name`, `price`) VALUES ('Item 2', 200);
-INSERT INTO `items` (`name`, `price`) VALUES ('Item 3', 300);
-INSERT INTO `items` (`name`, `price`) VALUES ('Item 4', 400);
-INSERT INTO `items` (`name`, `price`) VALUES ('Item 5', 600);
-INSERT INTO `items` (`name`, `price`) VALUES ('Item 6', 700);
-INSERT INTO `items` (`name`, `price`) VALUES ('Item 7', 800);
-INSERT INTO `items` (`name`, `price`) VALUES ('Item 8', 900);
-INSERT INTO `items` (`name`, `price`) VALUES ('Item 9', 100);
+INSERT INTO `items` (`name`, `price`, `image_url`) VALUES ('Item 1', 100, 'https://1.bp.blogspot.com/-eme6mXU1fN0/W8hDqW1o2ZI/AAAAAAABPeg/wrzinrwWGKQ40dPfhPpaXmSKmnRogJ81QCLcBGAs/s800/character_takenoko.png');
+INSERT INTO `items` (`name`, `price`, `image_url`) VALUES ('Item 2', 200, 'https://3.bp.blogspot.com/-SM2FKN6pNMM/W8hDqITydSI/AAAAAAABPec/jO2ycWVDZE08IHgxpQMW4iRxdyLi_SzTQCLcBGAs/s800/character_kinoko.png');
+INSERT INTO `items` (`name`, `price`, `image_url`) VALUES ('Item 3', 300, 'https://2.bp.blogspot.com/-Dxb0pCCF6cM/W5H_sSge7II/AAAAAAABOu4/OVJUv9PRaZw7YUJi3dBmStoC0r1q1v-nACLcBGAs/s800/body_character_syokumotsu_senni.png');
+INSERT INTO `items` (`name`, `price`, `image_url`) VALUES ('Item 4', 400, 'https://1.bp.blogspot.com/-rukduGqqqRo/W1vg37O9HJI/AAAAAAABNsE/7j5T-PAbfpEU7OlU6Lx4M52xVW1BPyzJQCLcBGAs/s800/animal_chara_donburi_tori.png');
+INSERT INTO `items` (`name`, `price`, `image_url`) VALUES ('Item 5', 600, 'https://2.bp.blogspot.com/-KXO_gm397xw/W1vg3syiIhI/AAAAAAABNsA/ZoZ40UmXYy0V9nucA1uUZwr--5pS2H_iwCLcBGAs/s800/animal_chara_donburi_buta.png');
+INSERT INTO `items` (`name`, `price`, `image_url`) VALUES ('Item 6', 700, 'https://3.bp.blogspot.com/-38ePZs54nsY/W1vg4jd5EmI/AAAAAAABNsI/3ALa8sVaFxoKXw0ldbd8K-X9oR_zPTjKACLcBGAs/s800/animal_chara_donburi_ushi.png');
+INSERT INTO `items` (`name`, `price`, `image_url`) VALUES ('Item 7', 800, 'https://4.bp.blogspot.com/-RHidPasFwTs/WwJZ3vBEuKI/AAAAAAABMIk/eTlXw8TVGSQQ2wPHpNAnmAucQ281DqFIgCLcBGAs/s800/food_character_udon.png');
+INSERT INTO `items` (`name`, `price`, `image_url`) VALUES ('Item 8', 900, 'https://4.bp.blogspot.com/-w3F1pR-usrk/WwJZ3Sne01I/AAAAAAABMIg/VU30MHeT_J4b-O2KlzdnFRkzeFxuR3SDQCLcBGAs/s800/food_character_soba.png');
+INSERT INTO `items` (`name`, `price`, `image_url`) VALUES ('Item 9', 100, 'https://3.bp.blogspot.com/-qql93jCouQg/WwJZ2nve8nI/AAAAAAABMIc/o8LZaDMK-PEL9H0SzWM0hOohpeOZ8L1sQCLcBGAs/s800/food_character_ra-men.png');
